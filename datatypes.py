d37ad3b199c4403d66790f8d63fd6786c37005b2
avyan44 <sumianubha@gmail.com>
2025-05-07 19:46:09 +0100

    This program is about datatypes

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+A=72
+print("The datatype of A is",type (A))
+B=2.5
+print("The datatype of B is",type(B))
+C=True 
+print("the datatype of C is",type(C))
+D="The dog is in the park"
+print("the datatype of D is",type(D))
